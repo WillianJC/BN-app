fc96d7fdf59ad0420e6663ecce63ef8f6f46de2f
WillianJC <193442453+WillianJC@users.noreply.github.com>
2026-06-04 20:17:52 -0500

    feat: integrate TypeORM and configuration management

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('PG_HOST'),
+        port: configService.get<number>('PG_PORT', 5432),
+        username: configService.get<string>('PG_USER'),
+        password: configService.get<string>('PG_PASSWORD'),
+        database: configService.get<string>('PG_DB'),
+        ssl:
+          configService.get<string>('PG_SSLMODE') === 'require'
+            ? { rejectUnauthorized: false }
+            : false,
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('PRODUCTION', 'false') !== 'true',
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
